2a4ee0b4261257858dd4d282f47a21c17a53281d
Jeffrey D Varner <jdv27@cornell.edu>
2026-03-19 13:02:56 -0400

    running

--- labs/week-9/L9d/Include.jl
+++ labs/week-9/L9d/Include.jl
@@ -6,25 +6,20 @@ const _PATH_TO_SRC = joinpath(_ROOT, "src");
 # check do we have a Manifest.toml file?
 using Pkg;
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
-     Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
+    Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 end
 
 using VLDataScienceMachineLearningPackage
-using Statistics
 using JLD2
 using LinearAlgebra
 using Plots
-using Distances
 using NNlib
-using Distributions
 using PrettyTables
-using DataFrames
 using StatsBase
 using Random
 
 # include my codes -
-include(joinpath(_PATH_TO_SRC, "BagOfWords.jl"));
 include(joinpath(_PATH_TO_SRC, "CBOW.jl"));
 include(joinpath(_PATH_TO_SRC, "SkipGram.jl"));
 include(joinpath(_PATH_TO_SRC, "NegativeSampling.jl"));
